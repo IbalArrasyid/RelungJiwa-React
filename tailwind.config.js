c6e10928a6dd14bbb4b9818ff40bafa18f9d099a
IbalArrasyid <iqbal.rasyid26@gmail.com>
2026-09-12 17:48:12 +0700

    Refresh branding, metadata, and site navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,2 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        brand: {
-          50: '#ecfeff',
-          100: '#cffafe',
-          200: '#a5f3fc',
-          300: '#67e8f9',
-          400: '#22d3ee',
-          500: '#06b6d4',
-          600: '#0891b2',
-          700: '#0e7490',
-          800: '#155e75',
-          900: '#164e63',
-        }
-      }
-    },
-  },
-  plugins: [],
-}
-
-
+export default {content:['./index.html','./src/**/*.{js,jsx,ts,tsx}'],theme:{extend:{colors:{cream:'#F8F3EC',espresso:'#292321',cocoa:'#815C4D',coral:'#D94843','sky-blue':'#8FC4DE','soft-blue':'#E2F0F6','soft-blush':'#F5E1E3',sage:'#849578',brand:{50:'#F8F3EC',500:'#815C4D',600:'#815C4D',700:'#604237'}}}},plugins:[]}
